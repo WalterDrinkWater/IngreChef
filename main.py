13341b00ef0e7504b772700fe0c73a0885dcd7bb
WalterDrinkWater <qingshui9362@gmail.com>
2023-11-23 22:32:20 +0800

    q

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ from tfidf_Vectorizer import TfidfEmbeddingVectorizer
 from preprocessing import ingredient_parser,get_and_sort_corpus
 from recommender import get_recommendations
 
-# model = Word2Vec.load(config.MODEL)
-# model.init_sims(replace=True)
+model = Word2Vec.load(config.MODEL)
+model.init_sims(replace=True)
 
-# data = pd.read_csv(config.RECIPES_DETAILS)
-# data["parsed"] = data.ingredients.apply(ingredient_parser)
-# corpus = get_and_sort_corpus(data)
-# tfidf_vec_tr = TfidfEmbeddingVectorizer(model)
-# tfidf_vec_tr.fit(corpus)
-# doc_vec = tfidf_vec_tr.transform(corpus)
-# doc_vec = [doc.reshape(1, -1) for doc in doc_vec]
+data = pd.read_csv(config.RECIPES_DETAILS)
+data["parsed"] = data.ingredients.apply(ingredient_parser)
+corpus = get_and_sort_corpus(data)
+tfidf_vec_tr = TfidfEmbeddingVectorizer(model)
+tfidf_vec_tr.fit(corpus)
+doc_vec = tfidf_vec_tr.transform(corpus)
+doc_vec = [doc.reshape(1, -1) for doc in doc_vec]
 
 input = "garlic, butter,te  s t"
 
 input = input.split(", ")
 stripped = [s.strip() for s in input]
 
-# input_embedding = tfidf_vec_tr.transform([input])[0].reshape(1, -1)
-# cos_sim = map(lambda x: cosine_similarity(input_embedding, x)[0][0], doc_vec)
+input_embedding = tfidf_vec_tr.transform([input])[0].reshape(1, -1)
+cos_sim = map(lambda x: cosine_similarity(input_embedding, x)[0][0], doc_vec)
 
-# scores = list(cos_sim)
-# recommendations = get_recommendations(5, scores)
+scores = list(cos_sim)
+recommendations = get_recommendations(5, scores)
 
-print(input) 
+print(recommendations) 
