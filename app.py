ad7a95ac05109f7bdffa9799dd507f069f59a7f8
WalterDrinkWater <qingshui9362@gmail.com>
2023-12-08 22:11:39 +0800

    final

--- app.py
+++ app.py
@@ -13,10 +13,19 @@ import pandas as pd
 import config
 from gensim.models import Word2Vec
 from sklearn.metrics.pairwise import cosine_similarity
-from tfidf_Vectorizer import TfidfEmbeddingVectorizer
-from preprocessing import ingredient_parser,get_and_sort_corpus
-from recommender import get_recommendations
+from recommendation_system.tfidf_Vectorizer import TfidfEmbeddingVectorizer
+from recommendation_system.preprocessing import ingredient_parser,get_and_sort_corpus
+from recommendation_system.recommender import get_recommendations
+from pymysql import connections, cursors
 
+db_conn = connections.Connection(
+    host=config.customhost,
+    port=3306,
+    user=config.customuser,
+    password=config.custompass,
+    connect_timeout=600,
+    db=config.customdb
+)
 app = Flask(__name__)
 
 @app.route("/")
@@ -37,7 +46,7 @@ def Recipe():
     doc_vec = [doc.reshape(1, -1) for doc in doc_vec]
 
     #clean input
-    input = request.form['ingredients']
+    input = request.form['ingredients'].lower()
     input = input.split(",")
     iIngred = [s.strip() for s in input]
     
@@ -46,12 +55,24 @@ def Recipe():
 
     scores = list(cos_sim)
     recommendations = get_recommendations(5, scores)
-    return render_template('Recipes.html', reco = recommendations)
+
+    return render_template('Recipes.html', reco = recommendations, ingred = iIngred)
 
 @app.route("/recipeDetails/<recipeName>")
 def RecipeDetails(recipeName):
-    print(recipeName)
-    return render_template('RecipeDetails.html')
+    cursor = db_conn.cursor()
+    cursor.execute("SELECT * FROM recipes WHERE recipe_name=%s LIMIT 1", (recipeName))
+    recipeDetails = cursor.fetchone()
+    ingredients = strToCleanList(recipeDetails[3])
+    steps = strToCleanList(recipeDetails[4])
+    return render_template('RecipeDetails.html',recipeDetails = recipeDetails, ingredients = ingredients, steps = steps)
+
+def strToCleanList(str):
+    str = str[1:len(str)-1]
+    splited_list = str.split("', '")
+    splited_list[0] = splited_list[0][1:len(splited_list[0])]
+    splited_list[len(splited_list)-1] = splited_list[len(splited_list)-1][0:len(splited_list[len(splited_list)-1])-1]
+    return splited_list
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pandas as pd
 import config
 from gensim.models import Word2Vec
 from sklearn.metrics.pairwise import cosine_similarity
-from tfidf_Vectorizer import TfidfEmbeddingVectorizer
-from preprocessing import ingredient_parser,get_and_sort_corpus
-from recommender import get_recommendations
+from recommendation_system.tfidf_Vectorizer import TfidfEmbeddingVectorizer
+from recommendation_system.preprocessing import ingredient_parser,get_and_sort_corpus
+from recommendation_system.recommender import get_recommendations
 
 model = Word2Vec.load(config.MODEL)
 model.init_sims(replace=True)
@@ -17,15 +17,16 @@ tfidf_vec_tr.fit(corpus)
 doc_vec = tfidf_vec_tr.transform(corpus)
 doc_vec = [doc.reshape(1, -1) for doc in doc_vec]
 
-input = "garlic, butter,te  s t"
+#clean input
+input = "onion, garlic, butter"
+input = input.split(",")
+iIngred = [s.strip() for s in input]
 
-input = input.split(", ")
-stripped = [s.strip() for s in input]
-
-input_embedding = tfidf_vec_tr.transform([input])[0].reshape(1, -1)
+input_embedding = tfidf_vec_tr.transform([iIngred])[0].reshape(1, -1)
 cos_sim = map(lambda x: cosine_similarity(input_embedding, x)[0][0], doc_vec)
 
 scores = list(cos_sim)
 recommendations = get_recommendations(5, scores)
 
-print(recommendations) 
+for ingre in recommendations["ingredients"]:
+    print(ingre)
