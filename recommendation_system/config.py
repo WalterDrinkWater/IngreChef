3c94e8698f3cc9cd707d5d6ed686806fb615560c
WalterDrinkWater <qingshui9362@gmail.com>
2023-12-09 22:33:08 +0800

    add padding

--- recommendation_system/config.py
+++ recommendation_system/config.py
@@ -1,3 +1,8 @@
 RECIPE_URLS = "./recommendation_system/dataset/Full_recipesDetails.csv"
 RECIPES_DETAILS = "./recommendation_system/dataset/Full_recipesDetails.csv"
 MODEL = "./recommendation_system/model_cbow.bin"
+customhost = "ingrechef.c9ykhllwofkw.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "IngreChef"
+customregion = "us-east-1"
